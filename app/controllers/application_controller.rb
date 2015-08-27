da957ef3b983030357c9f56b95de18d99c6cfaf2
Chuan He <chuan_he@icloud.com>
2015-08-27 05:03:59 +0000

    typo fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render test: "hello, world!"
+    render text: "hello, world!"
     end
   end
 end
